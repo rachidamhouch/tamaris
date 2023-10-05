7d6c9960567c94d96a773a179659243407ae5541
Rachid amhouch <rachid.amhouch@gmail.com>
2023-10-05 03:59:37 +0100

    front end

--- routes/home.js
+++ routes/home.js
@@ -6,4 +6,8 @@ router.get("/",async (req, res) => {
     res.render("home.ejs")
 })
 
+router.post("/done",async (req, res) => {
+    console.log(req.body)
+    res.render("home.ejs")
+})
 export default router
