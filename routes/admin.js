a17aa0f8794c30bad9b84f408b9a129de9aa9a39
Rachid amhouch <rachid.amhouch@gmail.com>
2023-10-06 02:44:29 +0100

    +pagination

--- routes/admin.js
+++ routes/admin.js
@@ -5,16 +5,21 @@ import User from "../models/user.js"
 
 const router = Router()
 
+
 router.get("/admin",async (req, res) => {
     if (req.isAuthenticated())
     {
+        const num = 6
+        const page = parseInt(req.query.page) || 0
         const user = await User.findById(req.user.id)
         if (user.admin == "false")
             return res.redirect("/")
-        res.render("admin.ejs")
+        const users = await User.find({googleId: null}).skip(page * num).limit(num)
+        res.render("admin.ejs", {users, size: await User.count({googleId: null}), page, num, next: page+1, prev: page - 1})
     }
     else
         res.redirect("/login")
 })
+
 
 export default router
